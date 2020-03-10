51a1ba976080293f48f0ed3c3704694c2d0796be
David E Lopez Diaz <lodi.dev@outlook.com>
2020-03-10 00:24:03 -0600

    checar lo de nueva cuenta que pasa

--- cliente/src/components/auth/NuevaCuenta.js
+++ cliente/src/components/auth/NuevaCuenta.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const NuevaCuenta = ()=>{
-    return(
-        <h1>NuevaCuenta</h1>
+
+const NuevaCuenta = () => {
+    //State oara ubucuar sesiòn
+    const [usuario, setUsuario] = useState({
+        email: '',
+        password: ''
+    })
+    //extraer de usuario
+    const { email, password } = usuario;
+
+    const onChange = (e) => {
+        setUsuario({
+            //crea una copia
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+    //iniciar
+    const onSubmit = e => {
+        e.preventDefault();
+        //Validar campos
+
+        //Pasar Action a reducer
+    }
+
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Inicia sesiòn</h1>
+                <form
+                    onSubmit={onSubmit}>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu nombre"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar sesiòn" />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
 
